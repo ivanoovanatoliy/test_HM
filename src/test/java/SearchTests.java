29a33cd686c4bde83a8fd7792bb316790e959b5a
ivanoovanatoliy <ivanoov.anatoly@gmail.com>
2025-01-21 20:05:41 +0500

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-         Configuration.pageLoadStrategy = "eager";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
