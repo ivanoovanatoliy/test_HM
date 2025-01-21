727151d53652607959d05da21c00f3ab904439a3
Anatoliy <ivanoov.anatoly@gmail.com>
2025-01-21 20:06:20 +0500

    add timeout

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-         Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 9000;
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
